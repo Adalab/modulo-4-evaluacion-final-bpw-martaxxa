f24abbf09fe4927a238e4a79804abfbb556a5c88
martaxxa <mchacartegui29@gmail.com>
2025-03-15 17:29:12 +0100

    Add first endpoint and API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 // Bibliotecas
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+async function getConnection() {
+  const connectionData = {
+    host: process.env["MYSQL_HOST"],
+    port: process.env["MYSQL_PORT"],
+    user: process.env["MYSQL_USER"],
+    password: process.env["MYSQL_PASS"],
+    database: process.env["MYSQL_SCHEMA"],
+  };
+  console.log(connectionData);
+
+  const connection = await mysql.createConnection(connectionData);
+  await connection.connect();
+  return connection;
+}
+
+// Configuración
 const app = express();
 
 app.use(cors());
@@ -13,6 +32,22 @@ app.listen(port, () => {
 });
 
 // Endpoints
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/rugby-femenino", async (req, res) => {
+
+  const conn = await getConnection();
+
+  const [teams] = await conn.query("SELECT * FROM teams;");
+  const [leagues] = await conn.query("SELECT * FROM leagues;");
+  const [players] = await conn.query("SELECT * FROM players;");
+
+  await conn.end();
+
+  res.json({
+    info: {
+      teams_count: teams.length,
+      leagues_count: leagues.length,
+      players_count: players.length,
+    },
+    results: {teams, leagues, players,}
+  });
 });
